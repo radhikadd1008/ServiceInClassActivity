095bee229b9746ae2606b6b85101695f3a8673c0
radhikadd1008 <radhikadd1008@gmail.com>
2023-04-13 15:01:55 -0400

    Services

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,49 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import java.sql.Time
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder : TimerService.TimerBinder
+    var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+                timerBinder = service as TimerService.TimerBinder
+                isConnected = true
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                isConnected = false
+            }
+
+        }
+
+        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) timerBinder.start(100)
 
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (!isConnected) timerBinder.stop()
         }
     }
 }
